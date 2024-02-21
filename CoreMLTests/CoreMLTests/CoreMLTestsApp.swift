9661791d76b2a29d2dd3922e34edfdfb48397b6d
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-02-21 16:38:52 -0300

    Ajustes Iniciais

---
+++ CoreMLTests/CoreMLTests/CoreMLTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLTestsApp.swift
+//  CoreMLTests
+//
+//  Created by Guilherme Nunes Lobo on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
